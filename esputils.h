4c13cfd4e8849ce641d11edc633894237d15dcbf
0xFEEDC0DE64 <daniel@brunner.ninja>
2021-01-21 23:12:44 +0100

    New literals and removed wifi related stuff

--- esputils.h
+++ esputils.h
@@ -95,6 +95,62 @@ private:
     std::vector<Slot> m_slots;
 };
 
+namespace literals {
+namespace {
+/**
+ * User-defined Literals
+ *  usage:
+ *
+ *   uint32_t = test = 10_MHz; // --> 10000000
+ */
+
+constexpr unsigned long long operator"" _kHz(unsigned long long x)
+{
+    return x * 1000;
+}
+
+constexpr unsigned long long operator"" _MHz(unsigned long long x)
+{
+    return x * 1000 * 1000;
+}
+
+constexpr unsigned long long operator"" _GHz(unsigned long long x)
+{
+    return x * 1000 * 1000 * 1000;
+}
+
+constexpr unsigned long long operator"" _kBit(unsigned long long x)
+{
+    return x * 1024;
+}
+
+constexpr unsigned long long operator"" _MBit(unsigned long long x)
+{
+    return x * 1024 * 1024;
+}
+
+constexpr unsigned long long operator"" _GBit(unsigned long long x)
+{
+    return x * 1024 * 1024 * 1024;
+}
+
+constexpr unsigned long long operator"" _kB(unsigned long long x)
+{
+    return x * 1024;
+}
+
+constexpr unsigned long long operator"" _MB(unsigned long long x)
+{
+    return x * 1024 * 1024;
+}
+
+constexpr unsigned long long operator"" _GB(unsigned long long x)
+{
+    return x * 1024 * 1024 * 1024;
+}
+} // namespace
+} // namespace literals
+
 namespace {
 template<typename T>
 T vmin(T&&t)
