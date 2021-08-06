0b9e04db920d3f6a3f8e322e99b9c805799e9683
0xFEEDC0DE64 <daniel@brunner.ninja>
2021-08-06 17:10:51 +0200

    Fixed unique symbol name for result inside macro

--- src/espcppmacros.h
+++ src/espcppmacros.h
@@ -3,19 +3,19 @@
 namespace espcpputils {
 
 #define CALL_AND_EXIT_ON_ERROR(METHOD, ...) \
-    if (const auto result = METHOD(__VA_ARGS__); result != ESP_OK) \
+    if (const auto result##__LINE__ = METHOD(__VA_ARGS__); result##__LINE__ != ESP_OK) \
     { \
-        ESP_LOGE(TAG, "%s() failed with %s", #METHOD, esp_err_to_name(result)); \
-        return result; \
+        ESP_LOGE(TAG, "%s() failed with %s", #METHOD, esp_err_to_name(result##__LINE__)); \
+        return result##__LINE__; \
     }
 
 #define CALL_AND_EXIT(METHOD, ...) \
-    if (const auto result = METHOD(__VA_ARGS__); result != ESP_OK) \
+    if (const auto result##__LINE__ = METHOD(__VA_ARGS__); result##__LINE__ != ESP_OK) \
     { \
-        ESP_LOGE(TAG, "%s() failed with %s", #METHOD, esp_err_to_name(result)); \
-        return result; \
+        ESP_LOGE(TAG, "%s() failed with %s", #METHOD, esp_err_to_name(result##__LINE__)); \
+        return result##__LINE__; \
     } \
     else \
-        return result;
+        return result##__LINE__;
 
 } // namespace espcpputils
