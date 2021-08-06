1ef4a9ea561947fa327c59e54a0cef79f6817c46
0xFEEDC0DE64 <daniel@brunner.ninja>
2021-08-06 17:11:04 +0200

    Removed httpd utilities

--- src/espstrutils.cpp
+++ src/espstrutils.cpp
@@ -2,7 +2,6 @@
 
 // esp-idf includes
 #include <esp_log.h>
-#include <http_parser.h>
 
 // 3rdparty lib includes
 #include <fmt/core.h>
@@ -25,46 +24,6 @@ std::string toString(sntp_sync_mode_t val)
         ESP_LOGW(TAG, "unknown sntp_sync_mode_t(%i)", std::to_underlying(val));
         return fmt::format("Unknown sntp_sync_mode_t({})", std::to_underlying(val));
     }
-}
-
-void urldecode(char *dst, const char *src)
-{
-    while (*src) {
-        char a, b;
-        if ((*src == '%') && ((a = src[1]) && (b = src[2])) && (std::isxdigit(a) && std::isxdigit(b))) {
-            if (a >= 'a')
-                a -= 'a'-'A';
-            if (a >= 'A')
-                a -= ('A' - 10);
-            else
-                a -= '0';
-
-            if (b >= 'a')
-                b -= 'a'-'A';
-            if (b >= 'A')
-                b -= ('A' - 10);
-            else
-                b -= '0';
-
-            *dst++ = 16*a+b;
-            src+=3;
-        } else if (*src == '+') {
-            *dst++ = ' ';
-            src++;
-        } else
-            *dst++ = *src++;
-    }
-
-    *dst++ = '\0';
-}
-
-tl::expected<void, std::string> urlverify(std::string_view str)
-{
-    http_parser_url puri;
-    http_parser_url_init(&puri);
-    if (const auto result = http_parser_parse_url(str.data(), str.size(), 0, &puri); result != 0)
-        return tl::make_unexpected(fmt::format("http_parser_parse_url() failed parsing the url with {}", result));
-    return {};
 }
 
 } // namespace espcpputils
--- src/espstrutils.h
+++ src/espstrutils.h
@@ -6,15 +6,8 @@
 // esp-idf includes
 #include <esp_sntp.h>
 
-// 3rdparty lib includes
-#include <tl/expected.hpp>
-
 namespace espcpputils {
 
 std::string toString(sntp_sync_mode_t val);
-
-void urldecode(char *dst, const char *src);
-
-tl::expected<void, std::string> urlverify(std::string_view str);
 
 } // namespace espcpputils
